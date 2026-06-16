c8f0d79fdfb4cd0d2f543f32a52ae96915e491b8
abyzzboyxdd <zizyyn@gmail.com>
2026-06-16 12:11:32 +0300

    Delete mysql.sh

--- mysql.sh
+++
@@ -1,7 +0,0 @@
-cat <<EOF | sudo tee /etc/yum.repos.d/mariadb.repo
-[mariadb]
-name = MariaDB
-baseurl = https://rpm.mariadb.org/10.5/centos7-amd64
-gpgkey=https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
-gpgcheck=1
-EOF
